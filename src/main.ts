1c519d08d861734e356b2580210918ea8217628f
chinmareno <chinmareno1@gmail.com>
2025-12-16 21:20:24 +0700

    feat: add env for cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({ origin: process.env.CLIENT_URL! });
   const port = process.env.PORT ?? 3001;
   await app.listen(port);
   console.log('run on port ' + port);
